2484eb69f562d1726a7e800d64a4e50fd0c7639e
BillerPlay <abulyaa@gmail.com>
2026-03-06 14:46:12 +0400

    Course class is finished

---
+++ ironschool/src/main/java/com/ironhack/ironschool/model/Course.java
@@ -0,0 +1,63 @@
+package com.ironhack.ironschool.model;
+
+import java.util.Random;
+
+public class Course {
+    private String courseId = generateCourseId();
+    private String name;
+    private double price;
+    private double moneyEarned;
+    private Teacher teacher;
+
+    public Course(String name, double price) {
+        this.name = name;
+        this.price = price;
+        this.moneyEarned = 0;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getMoneyEarned() {
+        return moneyEarned;
+    }
+
+    public void setMoneyEarned(double moneyEarned) {
+        this.moneyEarned = moneyEarned;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+    public String generateCourseId(){
+        Random random = new Random();
+        String coursePrefix = "COU";
+        int number = 1000 + random.nextInt(9000);
+        return coursePrefix + "-" + number;
+    }
+}
